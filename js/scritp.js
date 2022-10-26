92b06c3a10c94527d6f6cc90760a5eb84e3ffb9a
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-26 18:22:39 +0200

    fine

--- js/scritp.js
+++ js/scritp.js
@@ -1,5 +1,5 @@
 "use strict";
-class LavoratoreAutonomo {
+class LavoratoriAutonomi {
     constructor(codred, redditoAnnuoLordo, tasseInps, tasseIrpef) {
         this.codred = codred;
         this.redditoAnnuoLordo = redditoAnnuoLordo;
@@ -7,3 +7,44 @@ class LavoratoreAutonomo {
         this.tasseIrpef = tasseIrpef;
     }
 }
+class Programmatore extends LavoratoriAutonomi {
+}
+class FrontEndDev extends Programmatore {
+    constructor(codred, redditoAnnuoLordo, tasseInps, tasseIrpef) {
+        super(codred, redditoAnnuoLordo, tasseInps, tasseIrpef);
+    }
+    getUtileTasse() {
+        return (this.redditoAnnuoLordo * this.codred) / 100;
+    }
+    getTasseInps() {
+        return (this.getUtileTasse() * this.tasseInps) / 100;
+    }
+    getTasseIrpef() {
+        return (this.getUtileTasse() * this.tasseIrpef) / 100;
+    }
+    getRedditoAnnuoNetto() {
+        return (this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef());
+    }
+}
+let mario = new FrontEndDev(40, 45000, 26, 23);
+console.log("Reddito tasse INps: " + mario.getTasseInps());
+console.log("Reddito tasse Irpef: " + mario.getTasseIrpef());
+console.log("Reddito annuo netto: " + mario.getRedditoAnnuoNetto());
+// {
+//     if (this.redditoAnnuoLordo <= 15000){
+//         console.log((this.redditoAnnuoLordo * 23) /100)
+//     } else if(this.redditoAnnuoLordo > 15000 && this.redditoAnnuoLordo <= 28000) {
+//         console.log((this.redditoAnnuoLordo * 25) /100)
+//     } else if(this.redditoAnnuoLordo > 28000 && this.redditoAnnuoLordo <= 50000) {
+//         console.log((this.redditoAnnuoLordo * 35) /100)
+//     }else if(this.redditoAnnuoLordo > 50000) {
+//         console.log((this.redditoAnnuoLordo * 45) /100)
+//     }
+// }
+// {
+//     if (this.codred == 1) {
+//         console.log((this.redditoAnnuoLordo * 25) /100)
+//     } else if (this.codred == 2) {
+//         console.log((this.redditoAnnuoLordo * 33) /100)
+//     }
+// }
--- ts/scritp.ts
+++ ts/scritp.ts
@@ -1,12 +1,67 @@
-abstract class LavoratoreAutonomo {
-    codred: number;
-    redditoAnnuoLordo: number;
-    tasseInps: number;
-    tasseIrpef: number;
+abstract class LavoratoriAutonomi {
+    protected codred: number;
+    protected redditoAnnuoLordo: number;
+    protected tasseInps: number;
+    protected tasseIrpef: number;
     constructor(codred: number, redditoAnnuoLordo: number,tasseInps: number, tasseIrpef: number ) {
         this.codred = codred;
         this.redditoAnnuoLordo = redditoAnnuoLordo;
         this.tasseInps = tasseInps;
         this.tasseIrpef = tasseIrpef;
     }
+    public abstract getUtileTasse(): number;
+    public abstract getTasseInps(): number;
+    public abstract getTasseIrpef(): number;
+
 }
+abstract class Programmatore extends LavoratoriAutonomi {
+    public abstract getRedditoAnnuoNetto(): number;
+}
+class FrontEndDev extends Programmatore {
+    constructor(codred: number, redditoAnnuoLordo: number,tasseInps: number, tasseIrpef: number) {
+        super(codred, redditoAnnuoLordo, tasseInps, tasseIrpef)
+    }
+    public getUtileTasse(): number {
+        return (this.redditoAnnuoLordo * this.codred)/100;
+    }
+    public getTasseInps(): number {
+        return (this.getUtileTasse() * this.tasseInps)/100;
+    }
+    public getTasseIrpef(): number {
+        return (this.getUtileTasse() * this.tasseIrpef)/100;
+    }
+    public getRedditoAnnuoNetto(): number {
+        return (this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef());
+    }
+
+}
+let mario = new FrontEndDev(40, 45000, 26, 23);
+console.log("Reddito tasse INps: " + mario.getTasseInps());
+console.log("Reddito tasse Irpef: " + mario.getTasseIrpef());
+console.log("Reddito annuo netto: " + mario.getRedditoAnnuoNetto());
+
+
+
+
+// {
+//     if (this.redditoAnnuoLordo <= 15000){
+//         console.log((this.redditoAnnuoLordo * 23) /100)
+//     } else if(this.redditoAnnuoLordo > 15000 && this.redditoAnnuoLordo <= 28000) {
+//         console.log((this.redditoAnnuoLordo * 25) /100)
+//     } else if(this.redditoAnnuoLordo > 28000 && this.redditoAnnuoLordo <= 50000) {
+//         console.log((this.redditoAnnuoLordo * 35) /100)
+//     }else if(this.redditoAnnuoLordo > 50000) {
+//         console.log((this.redditoAnnuoLordo * 45) /100)
+//     }
+// }
+
+
+
+
+// {
+//     if (this.codred == 1) {
+//         console.log((this.redditoAnnuoLordo * 25) /100)
+//     } else if (this.codred == 2) {
+//         console.log((this.redditoAnnuoLordo * 33) /100)
+//     }
+// }
